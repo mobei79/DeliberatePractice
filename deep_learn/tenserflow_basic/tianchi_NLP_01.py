216585a1c281506bdd122ea8657d33be613e5105
mobei79 <42600272+mobei79@users.noreply.github.com>
2022-02-18 14:27:31 +0800

    mix

--- deep_learn/tenserflow_basic/tianchi_NLP_01.py
+++ deep_learn/tenserflow_basic/tianchi_NLP_01.py
@@ -7,3 +7,27 @@
 @file :
 @desc :
 """
+"""
+data set 
+
+train_set.csv   训练集
+test_a.csv      测试集
+test_a_sample_submit.csv    测试集2
+
+"""
+tianchi_data_path = "D:\Program Files\pythonwork\dataset\DeliberatePractice\deep_learn\\tianchi\\"
+
+
+import pandas as pd
+train_df = pd.read_csv(tianchi_data_path+"train_set.csv",
+                       sep="\t",        # csv文件每列的分割符号 \t
+                       nrows=100)       # 读取行数
+print(train_df.head())
+
+"""
+数据分析
+    非结构化的数据不需要太多数据分析工作，看一下数据规律即可。如文本长度、类型分布、字符分布等等
+    
+"""
+train_df["text_len"] = train_df["text"].apply(lambda x:len(x.split()))
+print(train_df["text_len"].describe())
